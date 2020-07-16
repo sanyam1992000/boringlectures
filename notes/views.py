bbafbb52b5e2be5b3b888f9ef9c6ee135a10aaf9
Sanyam <sanyam19092000@gmail.com>
2020-07-16 07:55:07 +0530

    first

--- notes/views.py
+++ notes/views.py
@@ -1,91 +1,87 @@
-import os
 from django.shortcuts import render
 from . import models
 from django.core.files import File
-# Create your views here.
-
-# h = get_pdf.pdf("")
+from urllib.request import urlopen
+import requests
+import json
 
 def get_notes(request, content_id):
 
     content_id = str(content_id)
-    from urllib.request import urlopen
-    import requests
-    import json
 
-    # notesid = input("content id")
-    # content_id = str(notesid)
-    base_url = "https://lecturenotes.in/material/" + content_id
-    client = requests.Session()
+    if models.Notes.objects.filter(notes_id=content_id).exists():
+        notes = models.Notes.objects.get(notes_id=content_id)
+    else:
+        base_url = "https://lecturenotes.in/material/" + content_id
+        client = requests.Session()
 
-    cookie = "lnsref=bba61fdf6086c715ef5a2153f399aa40"
-    head = {
-        "Cookie": cookie,
-        'accept': 'text/html,application/xhtml+xml,application/xml',
-        'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
-        "TE": "Trailers"
-    }
-    r = client.get(base_url, headers=head)
-
-    # print(r.text)
-    data = r.text
-    st = (data.find('csrfToken'))
-    cst = (data.find('"', st, len(data) - 1))
-    end = data.find('"', cst + 1, cst + 100)
-    csrf = ""
-    for i in range(cst + 1, end):
-        csrf += (data[i])
-    # print(csrf)
-    page_no = 1
-    url = "https://lecturenotes.in/material/" + str(content_id) + "/page-" + str(page_no) + "?noOfItems=30"
-    token = "oG7nVEzv4_pjQ4CG2BfDYXjI5M1H-vnUVhZ-lYtRbv4"
-    cookie = "lnsref=bba61fdf6086c715ef5a2153f399aa40"
-    host = "lecturenotes.in"
-    head = {"X-CSRF-Token": csrf,
-            #      "Cookie":cookie,
-            "host": host,
+        cookie = "lnsref=bba61fdf6086c715ef5a2153f399aa40"
+        head = {
+            "Cookie": cookie,
             'accept': 'text/html,application/xhtml+xml,application/xml',
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
-            }
+            'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
+            "TE": "Trailers"
+        }
+        r = client.get(base_url, headers=head)
 
-    next_page = True
-
-    images = []
-    while (next_page):
+        # print(r.text)
+        data = r.text
+        st = (data.find('csrfToken'))
+        cst = (data.find('"', st, len(data) - 1))
+        end = data.find('"', cst + 1, cst + 100)
+        csrf = ""
+        for i in range(cst + 1, end):
+            csrf += (data[i])
+        # print(csrf)
+        page_no = 1
         url = "https://lecturenotes.in/material/" + str(content_id) + "/page-" + str(page_no) + "?noOfItems=30"
+        token = "oG7nVEzv4_pjQ4CG2BfDYXjI5M1H-vnUVhZ-lYtRbv4"
+        cookie = "lnsref=bba61fdf6086c715ef5a2153f399aa40"
+        host = "lecturenotes.in"
+        head = {"X-CSRF-Token": csrf,
+                #      "Cookie":cookie,
+                "host": host,
+                'accept': 'text/html,application/xhtml+xml,application/xml',
+                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+                }
 
-        data = client.get(url, headers=head)
-        # print(data.content)
-        d = json.loads(data.text)
-        if (len(d['page']) < 30):
-            next_page = False
+        next_page = True
 
-        img_base_url = "https://lecturenotes.in"
-        flag = 0
-        for pg in d['page']:
-            # print(pg['path'])
-            r = client.get(img_base_url + pg['path'], headers=head)
-            # print(r.url)
-            from PIL import Image
+        images = []
+        while (next_page):
+            url = "https://lecturenotes.in/material/" + str(content_id) + "/page-" + str(page_no) + "?noOfItems=30"
 
-            if flag > 0:
-                img = Image.open(urlopen(r.url))
-                im1 = img.convert('RGB')
-                images.append(img)
-                flag += 1
-            else:
-                img1 = Image.open(urlopen(r.url))
-                image1 = img1.convert('RGB')
+            data = client.get(url, headers=head)
+            # print(data.content)
+            d = json.loads(data.text)
+            if (len(d['page']) < 30):
+                next_page = False
 
-                flag += 1
+            img_base_url = "https://lecturenotes.in"
+            flag = 0
+            for pg in d['page']:
+                # print(pg['path'])
+                r = client.get(img_base_url + pg['path'], headers=head)
+                # print(r.url)
+                from PIL import Image
 
-        page_no += 30
+                if flag > 0:
+                    img = Image.open(urlopen(r.url))
+                    im1 = img.convert('RGB')
+                    images.append(img)
+                    flag += 1
+                else:
+                    img1 = Image.open(urlopen(r.url))
+                    image1 = img1.convert('RGB')
 
-    # print(images)
-    # print(type(images[0]))
-    image1.save(r'myImages.pdf', save_all=True, append_images=images)
-    print("##########################")
-    pdf = open('myImages.pdf', "rb+")
-    p1 = File(pdf)
-    notes = models.Notes.objects.create(notes_id=content_id, pdf=p1, title="Hello")
-    return render(request, "home.html")
+                    flag += 1
+
+            page_no += 30
+
+        image1.save(r'myImages.pdf', save_all=True, append_images=images)
+        pdf = open('{}.pdf'.format(content_id), "rb+")
+        p1 = File(pdf)
+        notes = models.Notes.objects.create(notes_id=content_id, pdf=p1, title="Hello")
+
+    context = {"notes": notes}
+    return render(request, "home.html", context)
