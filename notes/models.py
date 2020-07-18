b523354bc2202c19a34063d3671fc4cf4cdaa83a
Sanyam <sanyam19092000@gmail.com>
2020-07-18 22:31:43 +0530

    Celery working on Heroku

--- notes/models.py
+++ notes/models.py
@@ -6,6 +6,8 @@ class Notes(models.Model):
     notes_id = models.PositiveIntegerField()
     title = models.CharField(max_length=5000)
     pdf = models.FileField(blank=True, null=True, upload_to="notes")
+    date_time = models.DateTimeField(auto_now_add=True)
+    total_pages = models.PositiveIntegerField(default=0)
 
     class Meta:
         ordering = ['notes_id']
